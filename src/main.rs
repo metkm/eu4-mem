0918e8afb03165b1991c9207a5ceda152ce75039
Sibyl <metinkorkmaz417@gmail.com>
2022-04-02 23:22:44 +0300

    Little more usable

--- src/main.rs
+++ src/main.rs
@@ -16,31 +16,36 @@ fn delay() {
 fn main() {
     let tech_offsets = TechOffsets::default();
 
-    'process: loop {
+    loop {
         if let Some(game) = get_game() {
             let Game { handle, address } = game;
-            
             let tech_base = address + 0x02420FC8;
-            let admin_tech_address = add_offsets(&handle, &tech_base, &tech_offsets.admin);
-            let diplo_tech_address = add_offsets(&handle, &tech_base, &tech_offsets.diplo);
-            let military_tech_address = add_offsets(&handle, &tech_base, &tech_offsets.military);
             
-            'game: loop {
-                if handle.is_invalid() {
-                    println!("Game process is lost!");
-                    break 'process;
-                }
+            'handle: loop {
+                let admin_tech_address = add_offsets(&handle, &tech_base, &tech_offsets.admin);
 
-                let admin = get_value(&handle, &admin_tech_address);
-                if admin < 2 { // game is not loaded yet. Just skip it.
+                if get_value(&handle, &admin_tech_address) < 1 {
+                    println!("Probably game is not loaded yet. If it is try to open tech tab once.");
+                    delay();
                     continue;
                 }
-
-                println!("Admin Tech = {}", get_value(&handle, &admin_tech_address));
-                println!("Diplo Tech = {}", get_value(&handle, &diplo_tech_address));
-                println!("Military Tech = {}", get_value(&handle, &military_tech_address));
-
-                delay();
+                
+                let admin_tech_address = add_offsets(&handle, &tech_base, &tech_offsets.admin);
+                let diplo_tech_address = add_offsets(&handle, &tech_base, &tech_offsets.diplo);
+                let military_tech_address = add_offsets(&handle, &tech_base, &tech_offsets.military);
+                
+                loop {
+                    if get_value(&handle, &admin_tech_address) < 1 {
+                        println!("Lost process!");
+                        break 'handle;
+                    }
+    
+                    println!("Admin Tech = {}", get_value(&handle, &admin_tech_address));
+                    println!("Diplo Tech = {}", get_value(&handle, &diplo_tech_address));
+                    println!("Military Tech = {}", get_value(&handle, &military_tech_address));
+    
+                    delay();
+                }
             }
         } else {
             println!("Can't find eu4.exe process! Retrying..");
